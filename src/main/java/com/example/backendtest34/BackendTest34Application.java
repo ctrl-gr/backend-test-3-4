63926b4f0e0d9ea4c2612d4fe9c1f900f2a02881
ctrl-gr <giulia@giuliaredolfi.it>
2023-04-14 17:29:53 +0200

    Initial commit

---
+++ src/main/java/com/example/backendtest34/BackendTest34Application.java
@@ -0,0 +1,13 @@
+package com.example.backendtest34;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTest34Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTest34Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendtest34/BackendTest34ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendtest34;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTest34ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
